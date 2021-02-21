fdf8987b44f28fe71a1a847cafa9d3cab7857fca
Morlé Koudeka <ndimorle@gmail.com>
2021-02-21 15:59:27 +0000

    set togo url

--- keycloak-mobile-number-login-spi/build.sh
+++ keycloak-mobile-number-login-spi/build.sh
@@ -1,4 +1,6 @@
 #./mvnw clean install
 mkdir -p ../keycloak/providers && cp target/keycloak-mobile-number-login-spi-1.0-SNAPSHOT.jar ../keycloak/providers
+mkdir -p ../keycloak/themes
 cp -r themes/sys-admin ../keycloak/themes/
 cp -r themes/facility-operator ../keycloak/themes/
+cp -r themes/certificate-login ../keycloak/themes/
